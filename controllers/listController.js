00e7a76316481db4df7b7f9d3d296f275ee01e11
Tonyyalmeida <hi.nam.phan@gmail.com>
2017-11-03 15:45:04 +0700

    getWordsfromUserId works

--- controllers/listController.js
+++ controllers/listController.js
@@ -30,7 +30,7 @@ exports.create_a_list = function(req, res) {
 exports.update_a_list = function (req, res) {
 List.findOneAndUpdate({listId: req.body.listId}, {wordIds: req.body.wordIds}, function (err, doc) {
 if (err) return err;
-var response = Object.assign({doc}, { message: "hi"} );
+var response = Object.assign({doc}, { message: "hil3"} );
  res.json(response);
 })
 };
@@ -54,13 +54,16 @@ List.find({userId : req.params.userId}, function(err, list) {
 };
 
 exports.get_words_from_user = function (req, res) {
+  var tam = [];
 List.find({userId : req.params.userId}, function(err, list) {
     if (err)
      { res.send(err) };
-      var tam = [];
-      var drei = getWordsFromWordIds([28, 29, 30]).then(function(doc) {res.send(doc)});
+      var counter = 0;
+      var umdr = list.length;
+      loopThroughWordIdsArray(list, counter, umdr, tam, res);
   });
 }
+// response has  to be called in the callback to make sense;
 
 
 exports.get_words_from_list = function (req, res) {
@@ -70,9 +73,23 @@ Words.find({}).where('wordId').in(wordArray).exec(function (err, doc) {res.send(
 )}
 
 function getWordsFromWordIds (wordArray) {
-var h = Words.find({}).where('wordId').in(wordArray).exec();
+var wordThisArray = wordArray[0].split(',');
+var h = Words.find({}).where('wordId').in(wordThisArray).exec();
 return h;
 }
+
+function loopThroughWordIdsArray (wordIdsArray, counter, umdr, tam, res) {
+getWordsFromWordIds(wordIdsArray[counter].wordIds).then(
+function(doc) {
+  tam.push(doc);
+  counter += 1;
+if (counter < umdr) {
+loopThroughWordIdsArray(wordIdsArray, counter, umdr, tam, res);
+} else {
+res.json(tam)
+}
+})}
+
 
 //Words.find({}).where('wordId').in([28, 29]).exec(function (err, doc) {res.send(doc)})}
 
