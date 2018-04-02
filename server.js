c1318c8af5053ea8c052489417a9c947f2ccd5be
Tonyyalmeida <hi.nam.phan@gmail.com>
2018-04-03 06:16:52 +0700

    dummy requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3101,
   mongoose = require('mongoose'),
+  mongodb = require('mongodb').MongoClient;
   Task = require('./models/WordModel'), //created model loading here
   Task2 = require('./models/ListModel'), //created model loading here
   Users = require("./models/UserModel"),
@@ -10,16 +11,28 @@ var expressValidator = require('express-validator');
 var passport = require("passport");
 var passportJWT = require("passport-jwt");
 var LocalStrategy = require('passport-local').Strategy;
+const request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage. 
+  }
+});
+
 
 // mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/test',  {
+// mongoose.Promise = global.Promise;
+// db = connect("localhost:27017/myDatabase");
+
+mongoose.connect('mongodb://master:master@ds127300.mlab.com:27300/test123',  {
   useMongoClient: true,
   /* other options */
 }, function (err) {
   console.log('could not connect');
 }); 
 
+// mongodb.connect('mongodb://master:master@ds127300.mlab.com:27300/test123', function(err, db) {
+//  if (err) console.log(err);
+//   console.log("Connected correctly to server!")});
 //Standard for validator;
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
@@ -47,6 +60,14 @@ app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
    next();
 });
+
+app.use("/drei", function (req, res) {
+  request('http://www.google.com', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body) // Show the HTML for the Google homepage. 
+    }
+  });
+})
 
 var routes = require('./routes/routes')(app, passport); //importing route
 //routes(app); //register the route
