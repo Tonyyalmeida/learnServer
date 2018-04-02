4e601e0849ff89d26374acd8de0324447615c53b
Tonyyalmeida <hi.nam.phan@gmail.com>
2018-04-03 06:54:50 +0700

    fixed long request

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ var passport = require("passport");
 var passportJWT = require("passport-jwt");
 var LocalStrategy = require('passport-local').Strategy;
 const request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage. 
-  }
-});
 
 
 // mongoose instance connection url connection
